37d12af20aa4f0327681d66862e7f6c6e83eae23
Aloestylez <aloestylez@outlook.es>
2022-06-26 09:49:27 -0500

    Ubigeo modificaciones

--- src/main/java/com/empresa/service/UbigeoService.java
+++ src/main/java/com/empresa/service/UbigeoService.java
@@ -1,5 +1,11 @@
 package com.empresa.service;
 
-public interface UbigeoService {
+import java.util.List;
 
+import com.empresa.entity.Ubigeo;
+
+public interface UbigeoService {
+	public abstract List<String> listaDepartamentos();
+	public abstract List<String> listaProvincias(String departamento);
+	public abstract List<Ubigeo> listaDistritos(String departamento, String provincia);
 }
